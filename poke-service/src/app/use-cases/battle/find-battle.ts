653f20c197eddc345ea31f25efb4cab87bc5b9aa
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2023-03-04 16:51:32 -0300

    use-case find-battle created

---
+++ poke-service/src/app/use-cases/battle/find-battle.ts
@@ -0,0 +1,26 @@
+import { Battle } from '@app/entities/battle';
+import { BattleRepository } from '@app/repositories/battle-repository';
+import { Injectable } from '@nestjs/common';
+
+interface FindBattleResponse {
+  battle: Battle;
+}
+
+interface FindBattleRequest {
+  id: string;
+}
+
+Injectable();
+export class FindBattle {
+  constructor(private battleRepository: BattleRepository) {}
+
+  async execute(request: FindBattleRequest): Promise<FindBattleResponse> {
+    const { id } = request;
+
+    const battle = await this.battleRepository.find(id);
+
+    return {
+      battle,
+    };
+  }
+}
