8d9fa321a08ee089bb02b5d35d6f6252e77990a4
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2023-03-04 16:15:21 -0300

    use-case create-trainer created

---
+++ poke-service/src/app/use-cases/trainer/create-trainer.ts
@@ -0,0 +1,35 @@
+import { Age } from '@app/entities/age';
+import { Trainer } from '@app/entities/trainer';
+import { TrainerRepository } from '@app/repositories/trainer-repository';
+import { Injectable } from '@nestjs/common';
+
+interface CreateTrainerResponse {
+  trainer: Trainer;
+}
+
+interface CreateTrainerRequest {
+  localId: string;
+  name: string;
+  age: number;
+}
+
+Injectable();
+export class CreateTrainer {
+  constructor(private trainerRepository: TrainerRepository) {}
+
+  async execute(request: CreateTrainerRequest): Promise<CreateTrainerResponse> {
+    const { localId, name, age } = request;
+
+    const trainer = new Trainer({
+      localId,
+      name,
+      age: new Age(age),
+    });
+
+    await this.trainerRepository.create(trainer);
+
+    return {
+      trainer,
+    };
+  }
+}
