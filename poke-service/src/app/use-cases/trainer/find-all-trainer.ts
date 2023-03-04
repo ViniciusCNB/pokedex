ba865c6ae047b8bcfb743bb8c2824f38bcbb1716
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2023-03-04 16:38:19 -0300

    use-case find-all-trainer created

---
+++ poke-service/src/app/use-cases/trainer/find-all-trainer.ts
@@ -0,0 +1,20 @@
+import { Trainer } from '@app/entities/trainer';
+import { TrainerRepository } from '@app/repositories/trainer-repository';
+import { Injectable } from '@nestjs/common';
+
+interface FindAllTrainerResponse {
+  trainer: Trainer[];
+}
+
+Injectable();
+export class FindAllTrainer {
+  constructor(private trainerRepository: TrainerRepository) {}
+
+  async execute(): Promise<FindAllTrainerResponse> {
+    const trainer = await this.trainerRepository.findAll();
+
+    return {
+      trainer,
+    };
+  }
+}
