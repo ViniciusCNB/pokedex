478cbc26178ff1e8f71998fb5adc74451d9918a9
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2023-03-04 16:53:26 -0300

    use-case find-all-battle created

---
+++ poke-service/src/app/use-cases/battle/find-all-battle.ts
@@ -0,0 +1,20 @@
+import { Battle } from '@app/entities/battle';
+import { BattleRepository } from '@app/repositories/battle-repository';
+import { Injectable } from '@nestjs/common';
+
+interface FindAllBattleResponse {
+  battle: Battle[];
+}
+
+Injectable();
+export class FindAllBattle {
+  constructor(private battleRepository: BattleRepository) {}
+
+  async execute(): Promise<FindAllBattleResponse> {
+    const battle = await this.battleRepository.findAll();
+
+    return {
+      battle,
+    };
+  }
+}
