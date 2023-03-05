64cdcf324ea7022a8834b375d283968a57957bca
Vinícius <viniborg25@gmail.com>
2023-03-05 19:51:44 -0300

    Design update in add local modal

--- projeto-pokedex/src/components/AddLocalModal.tsx
+++ projeto-pokedex/src/components/AddLocalModal.tsx
@@ -13,41 +13,40 @@ const AddLocalModal = () => {
       <Dialog.Portal>
         <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
-        <Dialog.Content className="fixed bg-slate-400/90 py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[30rem] shadow-lg shadow-black/25">
-          <Dialog.Title className="text-4xl text-center font-bold mb-10">
-            Add a Local
+        <Dialog.Content className="fixed bg-slate-400/90 py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[30rem] h-[24rem] shadow-lg shadow-black/25">
+          <Dialog.Title className="bg-red-500 rounded-md py-2 text-2xl text-center font-extrabold mb-10">
+            ADD A LOCAL
           </Dialog.Title>
 
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-2"
-          >
-            <div className="flex flex-col mb-5">
-              <label htmlFor="" className="font-bold text-[16px]">
-                NAME
-              </label>
-              <input
-                type="text"
-                className="bg-gray-200 text-black rounded py-3 px-4 shadow-xl"
-                {...register("name")}
-              />
+          <form onSubmit={handleSubmit(onSubmit)} className="relative">
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-col mb-5">
+                <label htmlFor="" className="font-bold text-[16px]">
+                  NAME
+                </label>
+                <input
+                  type="text"
+                  className="bg-gray-200 text-black rounded py-3 px-4 shadow-xl"
+                  {...register("name")}
+                />
+              </div>
+
+              <div className="flex flex-col mb-5">
+                <label htmlFor="" className="font-bold text-[16px]">
+                  TYPE
+                </label>
+                <select
+                  {...register("type")}
+                  className="bg-gray-200 text-black py-3 px-4 rounded shadow-xl"
+                >
+                  <option value=""></option>
+                  <option value="city">City</option>
+                  <option value="area">Area</option>
+                </select>
+              </div>
             </div>
 
-            <div className="flex flex-col mb-5">
-              <label htmlFor="" className="font-bold text-[16px]">
-                TYPE
-              </label>
-              <select
-                {...register("type")}
-                className="bg-gray-200 text-black py-3 px-4 rounded shadow-xl"
-              >
-                <option value=""></option>
-                <option value="city">City</option>
-                <option value="area">Area</option>
-              </select>
-            </div>
-
-            <button className="bg-red-500 text-base font-bold rounded-md p-3 text-white">
+            <button className="bg-red-500 text-base font-bold rounded-md p-3 text-white hover:bg-red-700 shadow-md shadow-black/25 absolute right-0">
               ADD
             </button>
           </form>
