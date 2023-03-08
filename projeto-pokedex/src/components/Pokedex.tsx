fb57e27ade1a0e1bf1032cda8324f7df19d8a12a
Vinícius <viniborg25@gmail.com>
2023-03-08 19:57:27 -0300

    Fixing new scroll issue in pokedex

--- projeto-pokedex/src/components/Pokedex.tsx
+++ projeto-pokedex/src/components/Pokedex.tsx
@@ -27,7 +27,7 @@ const Pokedex = () => {
     <div className="bg-red-600 w-1/2 mx-auto h-4/6 my-7 rounded-[50px] py-12 shadow-md shadow-black/60">
       <div className="bg-slate-100 w-11/12 h-[550px] mx-auto p-5 rounded-lg">
         <PokedexMenu handleFilterText={handleFilterText} />
-        <div className="grid grid-cols-2 gap-4 h-[400px] overflow-auto scrollbar scrollbar-thin-rose-500">
+        <div className="grid grid-cols-2 gap-4 max-h-[400px] overflow-auto scrollbar scrollbar-thin-rose-500">
           {pokemons
             ?.filter((pokemon) => {
               return filterText.toLowerCase() === ""
