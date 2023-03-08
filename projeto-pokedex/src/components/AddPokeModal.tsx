846211409136c4abc6077d46c967d6765c8a0e42
Vinícius <viniborg25@gmail.com>
2023-03-07 23:57:11 -0300

    Fixing css issues in add poke modal

--- projeto-pokedex/src/components/AddPokeModal.tsx
+++ projeto-pokedex/src/components/AddPokeModal.tsx
@@ -13,12 +13,12 @@ const AddPokeModal = () => {
       <Dialog.Portal>
         <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
-        <Dialog.Content className="fixed bg-slate-400/90 py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[30rem] h-[36rem] shadow-lg shadow-black/25">
+        <Dialog.Content className="fixed bg-slate-400/90 py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[30rem] h-[36.5rem] shadow-lg shadow-black/25">
           <Dialog.Title className="bg-red-500 rounded-md py-2 text-2xl text-center font-extrabold mb-10">
             ADD A POKÉMON
           </Dialog.Title>
 
-          <form onSubmit={handleSubmit(onSubmit)} className="relative ">
+          <form onSubmit={handleSubmit(onSubmit)} className="relative">
             <div className="grid grid-cols-2 gap-2">
               <div className="flex flex-col mb-5">
                 <label htmlFor="" className="font-bold text-[16px]">
@@ -47,7 +47,7 @@ const AddPokeModal = () => {
                   WEIGHT
                 </label>
                 <input
-                  type="text"
+                  type="number"
                   className="bg-gray-200 text-black rounded py-3 px-4 shadow-xl"
                   {...register("weight")}
                 />
