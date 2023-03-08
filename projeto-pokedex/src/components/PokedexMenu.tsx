e94b47170baa42eca1731ca6c98560798445dd65
Vinícius <viniborg25@gmail.com>
2023-03-08 09:39:25 -0300

    Fixing the close drop down menu event issue

--- projeto-pokedex/src/components/PokedexMenu.tsx
+++ projeto-pokedex/src/components/PokedexMenu.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import DropDownMenu from "./DropDownMenu"
+import { ClickAwayListener } from "@mui/base"
 
 interface PokedexMenuProps {
   handleFilterText: (text: string) => void
@@ -8,27 +9,29 @@ interface PokedexMenuProps {
 const PokedexMenu = (props: PokedexMenuProps) => {
   const [open, setOpen] = useState(false)
   return (
-    <div className="mb-5 bg-white/75 rounded-lg py-5 px-3 shadow-lg shadow-black/15 flex justify-between">
-      <input
-        placeholder="Search Pokémon"
-        type="text"
-        className="bg-gray-200 rounded py-3 px-4"
-        onChange={(e) => {
-          props.handleFilterText(e.target.value)
-        }}
-      />
+    <ClickAwayListener onClickAway={() => setOpen(false)}>
+      <div className="mb-5 bg-white/75 rounded-lg py-5 px-3 shadow-lg shadow-black/15 flex justify-between">
+        <input
+          placeholder="Search Pokémon"
+          type="text"
+          className="bg-gray-200 rounded py-3 px-4"
+          onChange={(e) => {
+            props.handleFilterText(e.target.value)
+          }}
+        />
 
-      <div>
-        <button
-          className="rounded bg-red-500 p-3 text-white hover:bg-red-700 font-bold"
-          onClick={() => setOpen(!open)}
-        >
-          OPTIONS
-        </button>
+        <div>
+          <button
+            className="rounded bg-red-500 p-3 text-white hover:bg-red-700 font-bold"
+            onClick={() => setOpen(!open)}
+          >
+            OPTIONS
+          </button>
 
-        {open ? <DropDownMenu /> : ""}
+          {open ? <DropDownMenu /> : ""}
+        </div>
       </div>
-    </div>
+    </ClickAwayListener>
   )
 }
 
