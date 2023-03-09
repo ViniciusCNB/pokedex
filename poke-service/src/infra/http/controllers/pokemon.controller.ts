73444903e4efa583295e1919121f67ea14f562dd
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2023-03-09 09:54:59 -0300

    route http implemented to methods find and delete pokemon

--- poke-service/src/infra/http/controllers/pokemon.controller.ts
+++ poke-service/src/infra/http/controllers/pokemon.controller.ts
@@ -1,13 +1,18 @@
 import { CreatePokemon } from '@app/use-cases/pokemon/create-pokemon';
+import { DeletePokemon } from '@app/use-cases/pokemon/delete-pokemon';
 import { FindAllPokemon } from '@app/use-cases/pokemon/find-all-pokemon';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { FindPokemon } from '@app/use-cases/pokemon/find-pokemon';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
 import { CreatePokemonBody } from '../dtos/create-pokemon-body';
+import { FindPokemonBody } from '../dtos/find-pokemon-body';
 import { PokemonViewModel } from '../view-models/pokemon-view-model';
 
 @Controller('pokemon')
 export class PokemonsController {
   constructor(
     private createPokemon: CreatePokemon,
+    private deletePokemon: DeletePokemon,
+    private findPokemon: FindPokemon,
     private findAllPokemon: FindAllPokemon,
   ) {}
 
@@ -37,6 +42,36 @@ export class PokemonsController {
       });
 
       return PokemonViewModel.toCreate(pokemon);
+    } catch (error) {
+      return {
+        title: 'Error',
+        message: 'Created pokemon error!',
+        error: error,
+      };
+    }
+  }
+
+  @Delete('find')
+  async delete(@Body() body: FindPokemonBody) {
+    const { id } = body;
+
+    try {
+      const response = await this.deletePokemon.execute({ id });
+    } catch (error) {
+      return {
+        title: 'Error',
+        message: 'Delete pokemon error!',
+        error: error,
+      };
+    }
+  }
+
+  @Get('find')
+  async find(@Body() body: FindPokemonBody) {
+    const { id } = body;
+
+    try {
+      const { pokemon } = await this.findPokemon.execute({ id });
     } catch (error) {
       return {
         title: 'Error',
