f3ac5404f7df7dd970836b7743dffb4b9de8e6a3
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2023-03-09 13:01:51 -0300

    Modules updated

--- poke-service/src/infra/database/database.module.ts
+++ poke-service/src/infra/database/database.module.ts
@@ -1,7 +1,13 @@
+import { BattleRepository } from '@app/repositories/battle-repository';
+import { LocalRepository } from '@app/repositories/local-repository';
 import { PokemonRepository } from '@app/repositories/pokemon-repository';
+import { TrainerRepository } from '@app/repositories/trainer-repository';
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { DatabaseBattlesRepository } from './repositories/database-battles-repository';
+import { DatabaseLocalsRepository } from './repositories/database-locals-repository';
 import { DatabasePokemonsRepository } from './repositories/database-pokemons-repository';
+import { DatabaseTrainersRepository } from './repositories/database-trainers-repository';
 
 @Module({
   providers: [
@@ -10,7 +16,24 @@ import { DatabasePokemonsRepository } from './repositories/database-pokemons-rep
       provide: PokemonRepository,
       useClass: DatabasePokemonsRepository,
     },
+    {
+      provide: LocalRepository,
+      useClass: DatabaseLocalsRepository,
+    },
+    {
+      provide: TrainerRepository,
+      useClass: DatabaseTrainersRepository,
+    },
+    {
+      provide: BattleRepository,
+      useClass: DatabaseBattlesRepository,
+    },
   ],
-  exports: [PokemonRepository],
+  exports: [
+    PokemonRepository,
+    LocalRepository,
+    TrainerRepository,
+    BattleRepository,
+  ],
 })
 export class DatabaseModule {}
--- poke-service/src/infra/http/http.module.ts
+++ poke-service/src/infra/http/http.module.ts
@@ -1,12 +1,51 @@
+import { CreateBattle } from '@app/use-cases/battle/create-battle';
+import { DeleteBattle } from '@app/use-cases/battle/delete-battle';
+import { FindAllBattle } from '@app/use-cases/battle/find-all-battle';
+import { FindBattle } from '@app/use-cases/battle/find-battle';
+import { CreateLocal } from '@app/use-cases/local/create-local';
+import { FindAllLocal } from '@app/use-cases/local/find-all-local';
+import { FindLocal } from '@app/use-cases/local/find-local';
+import { UpdateLocal } from '@app/use-cases/local/update-local';
+import { DeletePokemon } from '@app/use-cases/pokemon/delete-pokemon';
 import { FindAllPokemon } from '@app/use-cases/pokemon/find-all-pokemon';
+import { FindPokemon } from '@app/use-cases/pokemon/find-pokemon';
+import { CreateTrainer } from '@app/use-cases/trainer/create-trainer';
+import { DeleteTrainer } from '@app/use-cases/trainer/delete-trainer';
+import { FindAllTrainer } from '@app/use-cases/trainer/find-all-trainer';
+import { FindTrainer } from '@app/use-cases/trainer/find-trainer';
 import { Module } from '@nestjs/common';
 import { CreatePokemon } from 'src/app/use-cases/pokemon/create-pokemon';
 import { DatabaseModule } from '../database/database.module';
+import { BattleController } from './controllers/battle.controller';
+import { LocalsController } from './controllers/local.controller';
 import { PokemonsController } from './controllers/pokemon.controller';
+import { TrainerController } from './controllers/trainer.controller';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [PokemonsController],
-  providers: [CreatePokemon, FindAllPokemon],
+  controllers: [
+    PokemonsController,
+    LocalsController,
+    TrainerController,
+    BattleController,
+  ],
+  providers: [
+    CreatePokemon,
+    FindAllPokemon,
+    FindPokemon,
+    DeletePokemon,
+    CreateLocal,
+    UpdateLocal,
+    FindLocal,
+    FindAllLocal,
+    CreateBattle,
+    DeleteBattle,
+    FindBattle,
+    FindAllBattle,
+    CreateTrainer,
+    DeleteTrainer,
+    FindTrainer,
+    FindAllTrainer,
+  ],
 })
 export class HttpModule {}
