9ddcac4511421d9aa7b1f7619a67f325f75a53ca
Vinícius <viniborg25@gmail.com>
2023-03-13 19:46:21 -0300

    Adding cors

--- poke-service/src/main.ts
+++ poke-service/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DatabaseService } from './infra/database/database.service';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const databaseService = app.get(DatabaseService);
   await databaseService.connectDatabase();
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
