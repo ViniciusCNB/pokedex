9ad99272b60c26cfee3af52e82f3210959aa0d32
Vinícius Borges <viniborg25@gmail.com>
2023-03-22 16:27:59 -0300

    Adding the pokemon type information to the card

--- projeto-pokedex/src/components/Card.tsx
+++ projeto-pokedex/src/components/Card.tsx
@@ -15,9 +15,14 @@ const Card = (props: CardProps) => {
     <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger className="p-3 rounded-lg h-[120px] grid grid-cols-2 shadow-lg shadow-black/15 bg-gradient-to-r from-blue-300/40 to-blue-600/40 hover:bg-blue-300 items-center justify-center">
         <img src={props.pokemon.imageurl} alt="" className="w-[100px] m-auto" />
-        <p className="text-black/80 text-[16px] font-bold">
-          {props.pokemon.name.toUpperCase()}
-        </p>
+        <div className="flex flex-col">
+          <p className="text-black/80 text-[16px] font-bold">
+            {props.pokemon.name.toUpperCase()}
+          </p>
+          <p className="text-black/70 text-[10px] font-bold">
+            {props.pokemon.type.toUpperCase()}
+          </p>
+        </div>
       </Dialog.Trigger>
       {open && (
         <Suspense>
